a2597116758393a829afd9c0c1e1f6511cae4cd8
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-15 18:08:45 +0500

    add contact

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+import json
 
-# Create your views here.
+# import models
+from .models import Contact, Address, Student
+
+
+def add_contact(request: HttpRequest) -> JsonResponse:
+    """add contact view"""
+    if request.method == "POST":
+        # get data from request
+        decoded_data = request.body.decode()
+        data = json.loads(decoded_data)
+
+        # insert data
+        contact = Contact.objects.create(
+            phone=data.get('phone', 0),
+            email=data.get('email', 0),
+        )
+
+        return JsonResponse({
+            'id': contact.id,
+            'phone': contact.phone,
+            'email': contact.email,
+        })
+    return JsonResponse({'status': 'method not allawed'})
--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import add_contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/add/', add_contact),
 ]
